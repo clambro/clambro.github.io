7077f40ba72710963a8b0fc05146bb7e5090dc91
clambro <clambro.git@gmail.com>
2025-09-15 21:24:32 -0700

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  site: 'https://clambro.github.io',
+  site: 'https://www.costantinos.dev',
   trailingSlash: 'never',
+
   image: {
     layout: 'constrained',
     responsiveStyles: true,
   },
+
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
     },
   },
+
+  integrations: [sitemap()],
 });
