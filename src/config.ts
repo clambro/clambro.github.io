950421e61d1b8551a85c6c5d6b4a415153d60254
clambro <clambro.git@gmail.com>
2025-09-22 19:54:44 -0700

    Add twitter links (#13)
    * Add links
    
    * Edit copy
    
    * Abstract commonalities
    
    * Improve mobile layout for header
    
    * Tidy up homepage layout
    
    * Make links more subtle
    
    * Copy changes
    
    * Style tweaks
    
    * Update config.ts

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  description: 'AI Engineer | Projects & Blog',
+  social: {
+    github: 'https://github.com/clambro',
+    linkedin: 'https://www.linkedin.com/in/costa-lambrinoudis',
+    twitter: 'https://x.com/Clambr0',
+    rss: '/feed.xml',
+  },
+};
