1473d58dbd6825255f33334a86c05ca25541e035
clambro <clambro.git@gmail.com>
2025-10-26 11:34:20 -0700

    Update blog_tag.ts

--- src/types/blog_tag.ts
+++ src/types/blog_tag.ts
@@ -13,6 +13,7 @@ export enum BlogTag {
   GENETIC_2048 = 'genetic-2048',
   POKEMON_AI = 'pokemon-ai',
   WEBSITE = 'website',
+  BLACK_HOLE_THESIS = 'black-hole-thesis',
 }
 
 export function validateBlogTag(tag: string): BlogTag {
