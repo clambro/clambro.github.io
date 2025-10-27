598ea973436f749b5887093407cc9cad754d4042
clambro <clambro.git@gmail.com>
2025-10-26 22:44:24 -0700

    Add og and twitter cards (#15)
    * Create open-graph-default.svg
    
    * vibing

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,13 @@
 export const siteConfig = {
+  title: 'Costa Lambrinoudis',
   description: 'AI Engineer | Projects & Blog',
+  siteUrl: 'https://www.costantinos.dev',
+  ogImage: '/open-graph-default.svg',
   social: {
     github: 'https://github.com/clambro',
     linkedin: 'https://www.linkedin.com/in/costa-lambrinoudis',
     twitter: 'https://x.com/Clambr0',
+    twitterHandle: '@Clambr0',
     rss: '/feed.xml',
   },
 };
