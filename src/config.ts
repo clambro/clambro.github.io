e5e03e83e6e2430aba34f9d77a4b1cf80e4cc597
clambro <clambro.git@gmail.com>
2025-10-26 23:02:17 -0700

    Twitter doesn't support svgs (#16)
    * Create open-graph-default.png
    
    * Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   title: 'Costa Lambrinoudis',
   description: 'AI Engineer | Projects & Blog',
   siteUrl: 'https://www.costantinos.dev',
-  ogImage: '/open-graph-default.svg',
+  ogImage: '/open-graph-default.png',
   social: {
     github: 'https://github.com/clambro',
     linkedin: 'https://www.linkedin.com/in/costa-lambrinoudis',
